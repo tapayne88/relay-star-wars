81a2814fa9233970d010bd7cb433be9473a92d24
Tom Payne <tpayne@fastmail.co.uk>
2020-05-23 18:02:52 +0100

    Configured relay environment and added runtime types

---
+++ src/fetchGraphQL.ts
@@ -0,0 +1,23 @@
+import { RequestParameters, Variables } from "relay-runtime";
+
+const fetchGraphQL = async (
+  text: RequestParameters["text"],
+  variables: Variables
+) => {
+  // Fetch data from GitHub's GraphQL API:
+  const response = await fetch("https://graphql.org/swapi-graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+};
+
+export default fetchGraphQL;
