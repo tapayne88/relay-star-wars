e2cccb936ba1588facf8c823bb5755af0e225f35
Tom Payne <tpayne@fastmail.co.uk>
2020-05-24 19:34:22 +0100

    Figured out how to specify port on local API

--- src/fetchGraphQL.ts
+++ src/fetchGraphQL.ts
@@ -5,7 +5,7 @@ const fetchGraphQL = async (
   variables: Variables
 ) => {
   // Fetch data from GitHub's GraphQL API:
-  const response = await fetch("http://localhost:46705", {
+  const response = await fetch("http://localhost:8081", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
