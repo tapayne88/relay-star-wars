c33234c237c9f21a4b9c69e793aab0b074b9c1f5
Tom Payne <thomas.payne@skybettingandgaming.com>
2020-05-28 12:30:37 +0100

    Added query cache

--- src/RelayEnvironment.ts
+++ src/RelayEnvironment.ts
@@ -4,16 +4,32 @@ import {
   RecordSource,
   Store,
   FetchFunction,
+  QueryResponseCache,
 } from "relay-runtime";
 import fetchGraphQL from "./fetchGraphQL";
 
+const FIVE_MINUTE = 1000 * 60 * 5;
+const cache = new QueryResponseCache({ size: 250, ttl: FIVE_MINUTE });
+
 // Relay passes a "params" object with the query name and text. So we define a helper function
 // to call our fetchGraphQL utility with params.text.
-const fetchRelay: FetchFunction = async (params, variables) => {
+const fetchRelay: FetchFunction = async (operation, variables) => {
+  const queryID = operation.name;
+  const cachedData = cache.get(queryID, variables);
+
+  if (cachedData !== null) return cachedData;
+
   console.log(
-    `fetching query ${params.name} with ${JSON.stringify(variables)}`
+    `fetching query ${operation.name} with ${JSON.stringify(variables)}`
   );
-  return fetchGraphQL(params.text, variables);
+
+  return fetchGraphQL(operation.text, variables).then((data) => {
+    if (operation.operationKind !== "mutation") {
+      cache.set(queryID, variables, data);
+    }
+
+    return data;
+  });
 };
 
 // Export a singleton instance of Relay Environment configured with our network function:
